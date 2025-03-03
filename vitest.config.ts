4c8b9c30411d231ec0264f0bd97cc30b2ab12223
exoego <ytatsuno.jp@gmail.com>
2025-03-03 13:37:31 +0900

    demo

--- vitest.config.ts
+++ vitest.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
       reporter: ['lcov'],
     },
     watch: true,
-    includeSource: ['__tests__/**/*.{js,ts}'],
+    includeSource: ['test/**/*.{js,ts}'],
     exclude: [
       ...configDefaults.exclude,
       '/node_modules/',
